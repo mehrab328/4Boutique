c817a56f0ab9fd51c5fe2e1f6e3dfe03473d4082
shahriarrifat7 <shahriarrifat.csecu@gmail.com>
2020-10-10 22:59:54 +0600

    added jquery for back to top

--- resources/js/jqscript.js
+++ resources/js/jqscript.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $('.js--scroll-to-next').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-next').offset().top}, 1000);
     });
+    
+     $('.js--scroll-to-top').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-top').offset().top}, 1000);
+    });
 });
